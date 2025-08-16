6cd820b87177e914877cd4bcd3218410eeca8fea
Alexander Ono <alexanderono7@gmail.com>
2025-08-16 14:37:55 -0700

    attempt 3

--- main.cpp
+++ main.cpp
@@ -1,14 +1,32 @@
 #include "tgaimage.h"
+#include <cstdio>
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
 const TGAColor blue  = TGAColor(0, 0,   255,   255);
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
-	for (float x = x0; x <= x1; x++) {
-		float t = (x-x0)/(float)(x1-x0);
-		int y = y0*(1.-t) + (y1*t);
-        image.set(x, y, color);
+	bool steep = false;
+    // if the line we are drawing is too steep, then temporarily transpose the image
+    if(std::abs(x0-x1) < std::abs(y0-y1)){
+        std::swap(x0, y0);
+        std::swap(x1, y1);
+        steep = true;
+    }
+    // make it left-to-right if it is not 
+    if (x0 > x1) { 
+        std::swap(x0, x1);
+        std::swap(y0, y1);
+    }
+    for (int x=x0; x<=x1; x++) {
+        float t = (x-x0)/(float)(x1-x0);
+        int y = y0*(1.-t) + y1*t;
+        // un-transpose the image if it was transposed
+        if (steep) {
+            image.set(y, x, color);
+        } else {
+            image.set(x, y, color);
+        }
     }
 }
 
